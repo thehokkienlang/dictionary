6f54a3e8c64052f9f75e9a7351f15e3af370bde3
thehokkienlang <hokkienlanguage@gmail.com>
2026-09-03 21:21:44 +0800

    Clean up component-based English glosses

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const DATA_URL = "public/data/hokkien-hanri-dict.json?v=20260903-search-pages";
+const DATA_URL = "public/data/hokkien-hanri-dict.json?v=20260903-phrase-glosses";
 const RESULTS_PER_PAGE = 10;
 
 const state = {
